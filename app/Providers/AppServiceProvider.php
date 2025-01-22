37f259c970f7653584faf133e330e7c1a91b7947
stefanvasilev2002 <vasilevstefan2002@gmail.com>
2025-01-22 01:19:27 +0100

    HTTPS fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
         Blade::component('layouts.app', 'app-layout');
 
     }
